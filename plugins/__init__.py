1b47f79795eeeb4f9104ce40be3937b304de2395
Zero Two <82008300+zero2ditf@users.noreply.github.com>
2021-06-22 17:45:18 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -68,14 +68,8 @@ def grt(seconds: int) -> str:
 
 
 KANGING_STR = [
-    "Using Witchery to kang this sticker...",
-    "Plagiarising hehe...",
-    "Inviting this sticker over to my pack...",
-    "Kanging this sticker...",
-    "Hey that's a nice sticker!\nMind if I kang?!..",
-    "Hehe me stel ur stiker...",
-    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
-    "Roses are red violets are blue, kanging this sticker so my pack looks cool",
-    "Imprisoning this sticker...",
-    "Mr.Steal-Your-Sticker is stealing this sticker... ",
+    "Cloning For Darling.....",
+    "Darling wait whats the hurry in kanging, hehe...",
+    "Adding Honey to your stickers and making it my darlings..",
+    "That sticker is not yours its now my Darlings",
 ]
